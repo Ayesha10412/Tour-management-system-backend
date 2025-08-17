69aa893adea568b5c49c3a28e2f8473d89e702bd
Your Name <k.bristi057@gmail.com>
2025-08-17 13:34:11 +0600

    added the sendResponse function

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -5,25 +5,37 @@ import httpStatus from "http-status-codes";
 import { UserServices } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
 import { success } from "zod";
+import sendResponse from "../../utils/sendResponse";
 
 const createUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const user = await UserServices.createUser(req.body);
-    res
-      .status(httpStatus.CREATED)
-      .json({ message: "User created successfully!!", user });
+    // res
+    //   .status(httpStatus.CREATED)
+    //   .json({ message: "User created successfully!!", user });
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.CREATED,
+      message: "User created successfully!!",
+      data: user,
+    });
   }
 );
 const getAllUsers = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const users = await UserServices.getAllUsers();
-    res
-      .status(httpStatus.OK)
-      .json({
-        success: true,
-        message: "All users retrieved successfully!!",
-        users,
-      });
+    const result = await UserServices.getAllUsers();
+    // res.status(httpStatus.OK).json({
+    //   success: true,
+    //   message: "All users retrieved successfully!!",
+    //   users,
+    // });
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.CREATED,
+      message: "User users retrieved successfully!!",
+      data: result.data,
+      meta: result.meta,
+    });
   }
 );
 
